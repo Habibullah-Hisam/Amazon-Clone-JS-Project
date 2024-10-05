2d7a68cb11571624d0be8f4f45205c722b7f36b9
Habibullah <habibullahhisam@gmail.com>
2024-10-05 11:47:25 +0600

    what do i say ?

--- course Projects/javascript-amazon-project-main/scripts/amazon-project.js
+++ course Projects/javascript-amazon-project-main/scripts/amazon-project.js
@@ -49,7 +49,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
@@ -63,12 +63,12 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productName = button.dataset.productName;
+    const productId = button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-      if(productName === item.productName) {
+      if(productId === item.productId) {
         matchingItem = item;
       }
     });
@@ -77,7 +77,7 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     } else {
       
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1
       });
     };
