3bdebec8df15395173c57a846bb7764ba5da0788
Habibullah <habibullahhisam@gmail.com>
2024-12-11 23:31:02 +0600

    Make delivery options interactive

--- course Projects/javascript-amazon-project-main/scripts/checkout.js
+++ course Projects/javascript-amazon-project-main/scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart, calculateCartQuantity, saveToStorage } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity, saveToStorage, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import { deliveryOptions } from "../data/deliveryOptions.js";
@@ -103,7 +103,9 @@ function deliveryOptionsHTML(matchimgProduct, cartItem) {
 
     let isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     html +=
-    `<div class="delivery-option">
+    `<div class="delivery-option js-delivery-option"
+      data-product-id="${matchimgProduct.id}"
+      data-delivery-option-id="${deliveryOption.id}">
       <input type="radio"
       ${isChecked ? 'checked':''}
         class="delivery-option-input"
@@ -202,5 +204,12 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     container.remove();
     document.querySelector('.checkout-middle-header-js').innerHTML =
     `Checkout (<a class="return-to-home-link" href="amazon.html">${calculateCartQuantity()} items</a>)`;
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId)
   });
 });
