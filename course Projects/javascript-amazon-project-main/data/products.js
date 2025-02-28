4b288fcd36569e8893e9b488bb9d395c85a1cb28
Habibullah <habibullahhisam@gmail.com>
2025-02-28 12:14:20 +0600

    Load products from the backend and rename  amazon-project.js to amazon.js

--- course Projects/javascript-amazon-project-main/data/products.js
+++ course Projects/javascript-amazon-project-main/data/products.js
@@ -87,6 +87,31 @@ class Clothing extends Product {
 // }
 // object3.method();
 
+
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetailds) => {
+      if (productDetailds.type === 'clothing') {
+        return new Clothing(productDetailds);
+      }
+      return new Product(productDetailds);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -752,3 +777,4 @@ export const products = [
   }
   return new Product(productDetailds);
 });
+*/
